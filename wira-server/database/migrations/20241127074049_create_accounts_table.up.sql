dce0dc5a286814da1757fb613876e18b695a7c2d
KsoonQuea <68235390+KsoonQuea@users.noreply.github.com>
2024-12-06 05:38:29 +0800

    remove goravel

--- wira-server/database/migrations/20241127074049_create_accounts_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE accounts (
-  id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  created_at datetime(3) NOT NULL,
-  updated_at datetime(3) NOT NULL,
-  PRIMARY KEY (id),
-  KEY idx_accounts_created_at (created_at),
-  KEY idx_accounts_updated_at (updated_at)
-) ENGINE = InnoDB DEFAULT CHARSET = ;
